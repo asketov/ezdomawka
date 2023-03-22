708c330339371517d71b3ed3af19fa4e5fdb233e
Maxim Sidorenko <94392000+Optimvfx@users.noreply.github.com>
2023-03-22 19:36:53 +0200

    Commit

---
+++ DAL/Entities/Configuration/FavorSubjectDataContextConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Entities.Configuration;
+
+public class FavorSubjectDataContextConfiguration : IEntityTypeConfiguration<FavorSubject>
+{
+    public void Configure(EntityTypeBuilder<FavorSubject> builder)
+    {
+        builder.HasKey(bc => new { bc.FavorSolutionId, bc.SubjectId });
+        
+        builder.HasOne(bc => bc.FavorSolution)
+            .WithMany(b => b.FavorSubjects)
+            .HasForeignKey(bc => bc.FavorSolutionId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+       builder.HasOne(bc => bc.Subject)
+            .WithMany(c => c.FavorSubjects)
+            .HasForeignKey(bc => bc.SubjectId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
