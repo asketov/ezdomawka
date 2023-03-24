ad4ecdb13782734bca949398baca882e73934300
Maxim Sidorenko <94392000+Optimvfx@users.noreply.github.com>
2023-03-24 17:59:17 +0200

    Commit

---
+++ BLL/Implementations/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using BLL.Interfaces;
+using Common.Configs;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace BLL.Implementations;
+
+public class EmailSender : IEmailSender
+{
+    private readonly EmailConfig _emailConfig;
+
+    public EmailSender(EmailConfig emailConfig)
+    {
+        _emailConfig = emailConfig;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string text)
+    {
+        var emailMessage = new MimeMessage();
+
+        emailMessage.From.Add(new MailboxAddress("ezdomawka", _emailConfig.Name));
+        emailMessage.To.Add(new MailboxAddress(email, email));
+        emailMessage.Subject = subject;
+        emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+        {
+            Text = text
+        };
+
+        using (var client = new SmtpClient())
+        {
+            await client.ConnectAsync("smtp.gmail.com", 465, true);
+            await client.AuthenticateAsync(_emailConfig.Name, _emailConfig.Password);
+            await client.SendAsync(emailMessage);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ BLL/Implementations/LocalEmailSender.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Interfaces;
+
+namespace BLL.Implementations;
+
+public class LocalEmailSender : IEmailSender
+{
+    private readonly Action<string> _writeLocalMessage;
+
+    public LocalEmailSender(Action<string> writeLocalMessage)
+    {
+        _writeLocalMessage = writeLocalMessage;
+    }
+
+    public Task SendEmailAsync(string email, string subject, string text)
+    {
+        _writeLocalMessage.Invoke($"Email: {email}" +
+                                  $"\nSubject {subject}" +
+                                  $"\nText {text}");
+        
+        return Task.CompletedTask;
+    }
+}
